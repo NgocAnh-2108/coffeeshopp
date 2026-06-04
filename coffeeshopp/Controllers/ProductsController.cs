9f9ae1b0f1e6f3f9b423f2e48b7274731217067f
Anh <hoangngocanh20005@gmail.com>
2026-06-04 21:58:20 +0700

    Add project files.

---
+++ coffeeshopp/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using coffeeshopp.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Shop()
+        {
+            return View(productRepository.GetAllProducts());
+        }
+      
+    }
+}
---
+++ coffeeshopp/Models/Services/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using coffeeshopp.Data;
+using coffeeshopp.Models.Interfaces;
+using coffeeshopp.Models;
+
+namespace CoffeShop.Models.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> ProductsList = new List<Product>()
+    {
+        new Product { Id = 1, Name = "America", Price = 25, Detail = "Name product", ImageUrl = "https://index.com" },
+        new Product { Id = 2, Name = "Vietnam", Price = 20, Detail = "Vietnamese product", ImageUrl = "https://index.com" },
+        new Product { Id = 3, Name = "United Kingdom", Price = 15, Detail = "Name product", ImageUrl = "https://index.com" }
+    };
+
+
+        private CoffeeshopDbContext dbContext;
+
+        public ProductRepository(CoffeeshopDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return dbContext.Products;
+        }
+
+        public Product? GetProductDetail(int id)
+        {
+            return dbContext.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Product> GetTrendingProducts()
+        {
+            return dbContext.Products.Where(p => p.IsTrendingProduct);
+        }
+    }
+}
